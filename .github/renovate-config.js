56cf4c4ac368572d1dbeea5063fa7a068751ea3f
magisk317 <93979778+magisk317@users.noreply.github.com>
2026-05-12 15:31:36 +0800

    ci: migrate dependency updates to renovate

---
+++ .github/renovate-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  platform: "github",
+  repositories: ["NapGram/NapGram"],
+  onboarding: false,
+  requireConfig: "required",
+  gitAuthor: "magisk317 <93979778+magisk317@users.noreply.github.com>",
+};
